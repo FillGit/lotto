374f2d2350d93952234b3edece6165bdfc6057dc
DmitryEfremov <dmitry.efremov@quantumsoft.ru>
2022-01-29 17:01:13 +0700

    lotto-3 Added lotto_app -> app.

--- lotto_project/app/views.py
+++ lotto_project/app/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from lotto_project.lotto_app.serializers import UserSerializer, GroupSerializer
+from lotto_project.app.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
