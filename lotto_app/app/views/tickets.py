3b5f4092698604e00347aa59c6cedb6f58ebec0d
DmitryEfremov <dmitry.efremov@quantumsoft.ru>
2022-03-12 15:21:29 +0700

    lotto-9_json_file Created the draft pc_choice/choice_ten_tickets/ endpoint. Refactoring TicketViewSet. Created json file function.

--- lotto_app/app/views/tickets.py
+++ lotto_app/app/views/tickets.py
@@ -3,18 +3,17 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from lotto_app.constants import RUS_LOTTO_URL, RUS_LOTTO_HEADERS
 
-from lotto_app.app.utils import tickets_from_stoloto, tickets_check_status_code, get_tickets
+from lotto_app.app.utils import tickets_from_stoloto, get_tickets
 
 class TicketViewSet(ViewSet):
 
     @action(detail=False, url_path='rus_tickets_from_stoloto', methods=['get'])
     def rus_tickets_from_stoloto(self, request):
-        response = tickets_from_stoloto(RUS_LOTTO_URL, RUS_LOTTO_HEADERS)
-        return Response(response.json(), status=response.status_code)
+        response_json = tickets_from_stoloto(RUS_LOTTO_URL, RUS_LOTTO_HEADERS)
+        return Response(response_json, status=200)
 
     @action(detail=False, url_path='dict_tickets', methods=['get'])
     def dict_tickets(self, request):
-        response = tickets_from_stoloto(RUS_LOTTO_URL, RUS_LOTTO_HEADERS)
-        response_json = tickets_check_status_code(response)
+        response_json = tickets_from_stoloto(RUS_LOTTO_URL, RUS_LOTTO_HEADERS)
         tickets = get_tickets(response_json)
         return Response(tickets, status=200)
