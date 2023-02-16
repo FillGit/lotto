851968b5865aa7d1b32c8aee96a2c5a53344843a
FillGit <dmitry.efremov@quantumsoft.ru>
2023-02-16 20:12:25 +0700

    lotto-22 Fix GameSerializer.

--- lotto_app/app/serializers.py
+++ lotto_app/app/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
 from lotto_app.app.models import Game, LottoTickets, StateNumbers
+from lotto_app.app.utils import record_correction_game_numbers
+from lotto_app.constants import MAX_NUMBERS_IN_LOTTO
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,9 +19,26 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class GameSerializer(serializers.ModelSerializer):
+    def validate_numbers(self, value):
+        list_numbers = record_correction_game_numbers(value)
+        check = [n for n in list_numbers if n.isnumeric() is False]
+        if check:
+            raise serializers.ValidationError(f"You don't have a numerical value {check}")
+        check = [n for n in list_numbers if len(n) > 2]
+        if check:
+            raise serializers.ValidationError(
+                f"There is a value that has more than 2 characters {check}")
+        if len(list_numbers) > 90:
+            raise serializers.ValidationError(
+                f"You have many more numbers than {MAX_NUMBERS_IN_LOTTO} {list_numbers}")
+        if len(list_numbers) != len(set(check)):
+            raise serializers.ValidationError(
+                f"You have same numbers {list_numbers}")
+        return " ".join(list_numbers)
+
     class Meta:
         model = Game
-        fields = ['game', 'numbers',]
+        fields = ['game', 'numbers']
 
 
 class StateNumberSerializer(serializers.ModelSerializer):
