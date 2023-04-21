cc852ddf21d30898420a5cd7fbec76d3d96ace14
FillGit <dmitry.efremov@quantumsoft.ru>
2023-04-21 16:24:59 +0700

    lotto-37 Fixed tests/helpers.py Created new tests: tests/test_app/view/game/test_value_previous_games.py

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,10 +3,27 @@ from random import shuffle
 from lotto_app.app.models import Game
 
 
+"""
+Default win_card and win_ticket of the GameFactory class:
+
+    default
+        "win_card": {"by_account": 35, "last_win_number_ticket": xxx}
+        "win_ticket": {"by_account": 60, "last_win_number_ticket": xxx}
+
+    default in_order_numbers = True
+        "win_card": {"by_account": 35, "last_win_number_ticket": 35}
+        "win_ticket": {"by_account": 60, "last_win_number_ticket": 60}
+
+    default get_game_auto_win_json()
+        "win_card": {"by_account": 37, "last_win_number_ticket": xxx}
+        "win_ticket": {"by_account": 62, "last_win_number_ticket": xxx}
+"""
+
+
 class GameFactory():
 
     def __init__(self, name_game='test_lotto1', amount_games=3, game_ids=[], numbers_in_lotto=90,
-                 no_numbers_in_lotto=3, fields_games=None, only_games_json=False):
+                 no_numbers_in_lotto=3, fields_games=None, in_order_numbers=False, only_games_json=False):
 
         self.name_game = name_game
         self.amount_games = amount_games
@@ -14,6 +31,7 @@ class GameFactory():
         self.numbers_in_lotto = numbers_in_lotto
         self.no_numbers_in_lotto = no_numbers_in_lotto
         self.fields_games = fields_games
+        self.in_order_numbers = in_order_numbers
         self.only_games_json = only_games_json
 
         if fields_games:
@@ -31,27 +49,34 @@ class GameFactory():
             return no_numbers
         return None
 
-    def get_fields(self, game_id, name_game, numbers_in_lotto, no_numbers_in_lotto):
-        list_numbers = list(range(1, numbers_in_lotto+1))
-        shuffle(list_numbers)
-        numbers = [n for n in list_numbers[0:numbers_in_lotto-no_numbers_in_lotto]]
+    def get_fields(self, game_id):
+        if self.in_order_numbers:
+            numbers = [n for n in range(1, self.numbers_in_lotto-self.no_numbers_in_lotto+1)]
+            last_win_number_card = 35
+            last_win_number_ticket = 60
+        else:
+            list_numbers = list(range(1, self.numbers_in_lotto+1))
+            shuffle(list_numbers)
+            numbers = [n for n in list_numbers[0:self.numbers_in_lotto-self.no_numbers_in_lotto]]
+            last_win_number_card = list_numbers[35]
+            last_win_number_ticket = list_numbers[60]
         return {
-            'name_game': name_game,
+            'name_game': self.name_game,
             'game_id': game_id,
             'numbers': numbers,
-            'last_win_number_card': list_numbers[35],
-            'last_win_number_ticket': list_numbers[60],
-            'no_numbers': self.get_no_numbers(numbers, numbers_in_lotto)
+            'last_win_number_card': last_win_number_card,
+            'last_win_number_ticket': last_win_number_ticket,
+            'no_numbers': self.get_no_numbers(numbers, self.numbers_in_lotto)
         }
 
     def get_fields_games(self):
         if not self.game_ids:
             game_objs = Game.objects.filter(name_game=self.name_game).all()
             start = 1 if not game_objs else int(game_objs.last().game_id)+1
-            game_ids = [_id for _id in range(start, start+self.amount_games)]
+            _game_ids = [_id for _id in range(start, start+self.amount_games)]
+            return [self.get_fields(_id) for _id in _game_ids]
 
-        return [self.get_fields(_id, self.name_game, self.numbers_in_lotto, self.no_numbers_in_lotto)
-                for _id in game_ids]
+        return [self.get_fields(_id) for _id in self.game_ids]
 
     def set_games_db(self):
         return Game.objects.bulk_create([Game(**fields) for fields in self.fields_games])
---
+++ tests/test_app/view/game/test_value_previous_games.py
@@ -0,0 +1,39 @@
+from django_webtest import WebTest
+from hamcrest import assert_that, calling, has_length, is_, is_not, raises
+
+from tests.helpers import GameFactory
+
+
+class ValuePreviousGamesTest(WebTest):
+    endpoint = '/game/value_previous_games/'
+
+    def setUp(self):
+        super(ValuePreviousGamesTest, self).setUp()
+
+    def test_happy_path_value_previous_games(self):
+        self.game_factory = GameFactory(amount_games=5, in_order_numbers=True)
+        params = {'how_numbers': 45,
+                  'previous_games': 5,
+                  'current_game': 5
+                  }
+        resp = self.app.get(self.endpoint, params=params)
+        assert_that(resp.json, has_length(90))
+        expected_result = {str(i): 5 for i in range(1, 46)}
+        expected_result.update({str(i): 0 for i in range(46, 91)})
+        assert_that(resp.json, is_(expected_result))
+
+    def test_happy_path_without_how_numbers(self):
+        self.game_factory = GameFactory(amount_games=5)
+        params = {'previous_games': 3,
+                  'current_game': 5
+                  }
+        resp = self.app.get(self.endpoint, params=params)
+        expected_result = {str(i): 3 for i in range(1, 61)}
+        expected_result.update({str(i): 0 for i in range(61, 91)})
+        assert_that(resp.json, has_length(90))
+        assert_that(resp.json.keys(), is_(expected_result.keys()))
+        assert_that(resp.json, is_not(expected_result))
+
+    def test_validate_not_params(self):
+        self.game_factory = GameFactory(amount_games=5)
+        assert_that(calling(self.app.get).with_args(self.endpoint, params={}), raises(TypeError))
