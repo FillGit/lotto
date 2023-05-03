9140f606e17e74893adb8279e62333f6ac1cee72
FillGit <dmitry.efremov@quantumsoft.ru>
2023-05-03 08:00:10 +0700

    lotto-39 Added test_happy_path_without_field_last_wins_in_one_game() test to ValuePreviousGamesTest (tests/test_app/view/test_value_previous_games.py).

--- tests/test_app/view/test_value_previous_games.py
+++ tests/test_app/view/test_value_previous_games.py
@@ -1,6 +1,7 @@
 from django_webtest import WebTest
 from hamcrest import assert_that, calling, has_length, is_, is_not, raises
 
+from lotto_app.app.models import Game
 from tests.helpers import GameFactory
 
 
@@ -35,3 +36,25 @@ class ValuePreviousGamesTest(WebTest):
         self.game_factory = GameFactory(amount_games=5)
         assert_that(calling(self.app.get).with_args(f'{self.endpoint}5/', params={}),
                     raises(TypeError))
+
+    def test_happy_path_without_field_last_wins_in_one_game(self):
+        self.game_factory = GameFactory(amount_games=3)
+        _fields_games = GameFactory(amount_games=1,
+                                    only_games_json=True).get_game_json()
+        GameFactory(fields_games=[{
+                        'name_game': _fields_games['name_game'],
+                        'game_id': _fields_games['game_id'],
+                        'numbers': _fields_games['numbers'],
+                        'no_numbers': _fields_games['no_numbers']
+                    }])
+        GameFactory(amount_games=1)
+
+        assert_that(Game.objects.count(), is_(5))
+
+        params = {'previous_games': 3}
+        resp = self.app.get(f'{self.endpoint}5/', params=params)
+        expected_result = {str(i): 3 for i in range(1, 61)}
+        expected_result.update({str(i): 0 for i in range(61, 91)})
+        assert_that(resp.json, has_length(90))
+        assert_that(resp.json.keys(), is_(expected_result.keys()))
+        assert_that(resp.json, is_not(expected_result))
