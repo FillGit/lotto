c6e4d81a28696507cab3205f1424e8e9859fbee1
FillGit <dmitry.efremov@quantumsoft.ru>
2023-05-11 13:48:31 +0700

    lotto-42 Created the /{ng}/research/{pk}/games_9_parts_into_win_ticket/{format} endpoint. Refactoring lotto_app/app/views/researchs.py, added the get_general_game_objs() method.

--- lotto_app/app/views/researchs.py
+++ lotto_app/app/views/researchs.py
@@ -15,11 +15,21 @@ class ResearchViewSet(viewsets.ModelViewSet):
         return Game.objects.filter(name_game=self.kwargs['ng']).order_by('game_id')
 
     def get_game_obj(self):
-        return Game.objects.get(game_id=self.kwargs['pk'])
+        return Game.objects.get(game_id=self.kwargs['pk'], name_game=self.kwargs['ng'])
+
+    def get_general_game_objs(self):
+        name_game = self.kwargs['ng']
+        return Game.objects.filter(
+            name_game=name_game,
+            last_win_number_card__isnull=False,
+            last_win_number_ticket__isnull=False
+        ).annotate(
+            game_id_int=Cast('game_id', output_field=IntegerField())
+        )
 
     @action(detail=True, url_path='comparison_win_ticket', methods=['get'])
     def comparison_win_ticket(self, request, ng, pk=None):
-        main_game_obj = self.get_queryset().get(game_id=pk)
+        main_game_obj = self.get_game_obj()
 
         main_list_win_numbers = main_game_obj.numbers[:60]
         dict_common_numbers = {}
@@ -36,7 +46,7 @@ class ResearchViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, url_path='comparison_parts_win_ticket', methods=['get'])
     def comparison_parts_win_ticket(self, request, ng, pk=None):
-        main_game_obj = self.get_queryset().get(game_id=pk)
+        main_game_obj = self.get_game_obj()
 
         how_comparison_games = int(request.query_params.get('how_comparison_games'))
         part_consists_of = int(request.query_params.get('part_consists_of'))
@@ -47,14 +57,10 @@ class ResearchViewSet(viewsets.ModelViewSet):
         for numbers in main_combination_win_ticket['numbers_in_row']:
             main_numbers_in_row.extend(numbers)
 
-        comparison_games_objs = Game.objects.filter(
-            name_game=ng,
-            last_win_number_card__isnull=False,
-            last_win_number_ticket__isnull=False
-        ).annotate(
-            game_id_int=Cast('game_id', output_field=IntegerField())
-        ).filter(game_id_int__lte=int(pk)-1, game_id_int__gte=int(pk)-10-how_comparison_games
-                 ).order_by('-game_id_int')[0:how_comparison_games]
+        comparison_games_objs = self.get_general_game_objs().filter(
+            game_id_int__lte=int(pk)-1,
+            game_id_int__gte=int(pk)-10-how_comparison_games
+            ).order_by('-game_id_int')[0:how_comparison_games]
 
         dict_comparisons = {_obj.game_id: [] for _obj in comparison_games_objs}
         for _obj in comparison_games_objs:
@@ -77,7 +83,7 @@ class ResearchViewSet(viewsets.ModelViewSet):
     @action(detail=True, url_path='search_win_ticket', methods=['get'])
     def search_win_ticket(self, request, ng, pk=None):
         last_win_number_ticket = int(request.query_params.get('last_win_number_ticket', None))
-        main_game_obj = self.get_queryset().get(game_id=pk)
+        main_game_obj = self.get_game_obj()
         main_set_win_numbers = {int(num) for num in main_game_obj.get_win_list(last_win_number_ticket)}
 
         ticket_ids = []
@@ -99,14 +105,10 @@ class ResearchViewSet(viewsets.ModelViewSet):
             return Response({"error": "query_params doesn't how_games"},
                             status=status.HTTP_400_BAD_REQUEST)
 
-        game_objs = Game.objects.filter(
-            name_game=ng,
-            last_win_number_card__isnull=False,
-            last_win_number_ticket__isnull=False
-        ).annotate(
-            game_id_int=Cast('game_id', output_field=IntegerField())
-        ).filter(game_id_int__lte=game_start, game_id_int__gte=game_start-how_games-5
-                 ).order_by('-game_id_int')[0:how_games]
+        game_objs = self.get_general_game_objs().filter(
+            game_id_int__lte=game_start,
+            game_id_int__gte=game_start-how_games-5
+            ).order_by('-game_id_int')[0:how_games]
 
         if pk not in [game_obj.game_id for game_obj in game_objs]:
             return Response({"error": f"game_id - {game_start} doesn't have in query"},
@@ -134,3 +136,41 @@ class ResearchViewSet(viewsets.ModelViewSet):
             dict_no_numbers['all_no_numbers_9_parts'] = dict(
                 sorted(dict_no_numbers['all_no_numbers_9_parts'].items(), key=lambda item: item[1]))
         return Response(dict_no_numbers, status=200)
+
+    @action(detail=True, url_path='games_9_parts_into_win_ticket', methods=['get'])
+    def games_9_parts_into_win_ticket(self, request, ng, pk):
+        game_start = int(pk)
+        how_games = int(request.query_params.get('how_games', 0))
+        if not game_start:
+            return Response({"error": "query_params doesn't game_start"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        if not how_games:
+            return Response({"error": "query_params doesn't how_games"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        game_objs = self.get_general_game_objs().filter(
+            game_id_int__lte=game_start,
+            game_id_int__gte=game_start-how_games-5
+            ).order_by('-game_id_int')[0:how_games]
+
+        if pk not in [game_obj.game_id for game_obj in game_objs]:
+            return Response({"error": f"game_id - {game_start} doesn't have in query"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        game_index_9_parts = {}
+        for game_obj in game_objs:
+            game_id = int(game_obj.game_id)
+            last_total_cost_numbers = GameViewSet.get_several_games_info(ng, game_id-1)['total_cost_numbers']
+            list_win_ticket = game_obj.get_win_list(game_obj.last_win_number_ticket)
+            game_index_9_parts[game_id] = index_9_parts(last_total_cost_numbers, list_win_ticket)
+
+        game_index_9_parts['all_game_index_9_parts'] = {}
+        for _game, _index_9_parts in game_index_9_parts.items():
+            for part, cost in _index_9_parts.items():
+                if part not in game_index_9_parts['all_game_index_9_parts']:
+                    game_index_9_parts['all_game_index_9_parts'][part] = cost
+                else:
+                    game_index_9_parts['all_game_index_9_parts'][part] += cost
+            game_index_9_parts['all_game_index_9_parts'] = dict(
+                sorted(game_index_9_parts['all_game_index_9_parts'].items(), key=lambda item: item[1]))
+        return Response(game_index_9_parts, status=200)
