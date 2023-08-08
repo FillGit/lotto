b63b3f0e35915c9265cb829aa6d59c587a64cbfd
FillGit <dmitry.efremov@quantumsoft.ru>
2023-08-08 20:37:40 +0700

    lotto-51 Created new Parser8AddNumbers class.

--- lotto_app/constants.py
+++ lotto_app/constants.py
@@ -1,3 +1,4 @@
+from lotto_app.app.parsers.parser_8_add_numbers import Parser8AddNumbers
 from lotto_app.app.parsers.parser_90_numbers import Parser90Numbers
 
 QUANTITY_TICKETS = 300
@@ -5,5 +6,6 @@ QUANTITY_TICKETS = 300
 MAX_NUMBERS_IN_LOTTO = 90
 
 LOTTO_PARSER = {
-    '90_numbers': Parser90Numbers
+    '90_numbers': Parser90Numbers,
+    '8_add_numbers': Parser8AddNumbers
 }
