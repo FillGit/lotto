107533077c70c78d4eda94bbf3bad2ab03d7258e
FillGit <dmitry.efremov@quantumsoft.ru>
2023-08-12 16:36:56 +0700

    lotto-54 Refactoring CombinationOptions8Add class and /{nk}/research_8_add/{pk}/combination_options/ endpoint. Created the InfoSequence8Add class and created /{nk}/research_8_add/{pk}/info_sequence/ endpoint.

--- lotto_app/app/commands/command_utils.py
+++ lotto_app/app/commands/command_utils.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 from django.db.models import IntegerField
 from django.db.models.functions import Cast
 
@@ -5,15 +7,15 @@ from lotto_app.app.models import Game
 from lotto_app.constants import COMBINATION_OPTIONS_8_ADD
 
 
-class CombinationOptions8Add():
+class Utils8Add():
     def __init__(self, name_game, main_game_id, how_games):
         self.name_game = name_game
         self.main_game_id = main_game_id
         self.how_games = how_games
 
-        self.game_objs = self.get_game_objs(name_game, main_game_id, how_games)
+        self.game_objs = self._get_game_objs(name_game, main_game_id, how_games)
 
-    def get_game_objs(self, name_game, main_game_id, how_games):
+    def _get_game_objs(self, name_game, main_game_id, how_games):
         return Game.objects.filter(
             name_game=name_game,
         ).annotate(
@@ -38,16 +40,57 @@ class CombinationOptions8Add():
                 previous_n = n
         return sorted(combination_8_add, reverse=True)
 
-    def _get_sum_combination_options(self, combination_options):
+    def get_game_combinations(self):
+        return {game_obj.game_id: self._get_combination_options_8_add(game_obj.numbers)
+                for game_obj in self.game_objs}
+
+
+class CombinationOptions8Add(Utils8Add):
+    def get_sum_combination_options(self, game_combinations=None):
+        if not game_combinations:
+            game_combinations = self.get_game_combinations()
+
         _sum_combination = {k: 0 for k, _ in COMBINATION_OPTIONS_8_ADD.items()}
         for name, seq in COMBINATION_OPTIONS_8_ADD.items():
-            for _, combination in combination_options.items():
+            for _, combination in game_combinations.items():
                 if combination == seq:
                     _sum_combination[name] += 1
         return {name: seq for name, seq in _sum_combination.items() if seq != 0}
 
-    def get_combination_options(self):
-        combination_options = {game_obj.game_id: self._get_combination_options_8_add(game_obj.numbers)
-                               for game_obj in self.game_objs}
-        combination_options.update(self._get_sum_combination_options(combination_options))
-        return combination_options
+
+class InfoSequence8Add(Utils8Add):
+    def get_info_sequence(self, sequence, game_obj, previous_game_id=None):
+        info = {}
+        if not previous_game_id:
+            previous_game_id = self.game_objs[0].game_id
+
+        if set(sequence).issubset(set((game_obj.numbers))):
+            info[game_obj.game_id] = game_obj.game_id
+            info['previous game_id'] = previous_game_id
+            info['difference'] = int(previous_game_id) - int(game_obj.game_id)
+        return info
+
+    def get_all_info_sequence(self, sequence, only_len_sequence=True):
+        info = []
+        len_sequence = len(sequence)
+        game_combinations = self.get_game_combinations()
+        game_objs_by_game_id = {_obj.game_id: _obj for _obj in self.game_objs}
+        previous_game_id = self.game_objs[0].game_id
+        for game_id, combination in game_combinations.items():
+            _inf = self.get_info_sequence(sequence,
+                                          game_objs_by_game_id[game_id],
+                                          previous_game_id)
+            if _inf and only_len_sequence and len_sequence in combination:
+                info.append(_inf)
+                previous_game_id = game_id
+            elif _inf and len_sequence >= max(combination):
+                info.append(_inf)
+                previous_game_id = game_id
+        return info
+
+    def get_info_difference(self, all_info_sequence):
+        differences = [_info['difference'] for _info in all_info_sequence]
+        return {'min_difference': min(differences),
+                'middle_difference': mean(differences),
+                'max_difference': max(differences),
+                'amount_sequence': len(all_info_sequence)}
--- lotto_app/app/views/research/research_8_add.py
+++ lotto_app/app/views/research/research_8_add.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from lotto_app.app.commands.command_utils import CombinationOptions8Add
+from lotto_app.app.commands.command_utils import CombinationOptions8Add, InfoSequence8Add
 from lotto_app.app.views.research.research import ResearchViewSet
 
 
@@ -27,7 +27,18 @@ class Research8AddViewSet(ResearchViewSet):
     def combination_options(self, request, ng, pk=None):
         game_start = int(pk)
         how_games = int(request.query_params.get('how_games', 0))
-        return Response(CombinationOptions8Add(ng,
-                                               game_start,
-                                               how_games).get_combination_options(),
-                        status=200)
+        combination_options = CombinationOptions8Add(ng, game_start, how_games)
+        game_combinations = combination_options.get_game_combinations()
+        game_combinations.update(combination_options.get_sum_combination_options(game_combinations))
+        return Response(game_combinations, status=200)
+
+    @action(detail=True, url_path='info_sequence', methods=['get'])
+    def info_sequence(self, request, ng, pk=None):
+        game_start = int(pk)
+        how_games = int(request.query_params.get('how_games', 0))
+        sequence = [int(sequence) for sequence in request.query_params.get('sequence').replace(' ', '').split(',')]
+        only_len_sequence = int(request.query_params.get('only_len_sequence', 1))
+        i_s = InfoSequence8Add(ng, game_start, how_games)
+        all_info_sequence = i_s.get_all_info_sequence(sequence, only_len_sequence)
+        all_info_sequence.append(i_s.get_info_difference(all_info_sequence))
+        return Response(all_info_sequence, status=200)
